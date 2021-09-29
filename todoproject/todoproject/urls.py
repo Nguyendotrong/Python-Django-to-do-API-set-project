a1c2631472a1892dda48d49ee882959eeac18bf4
Nguyendotrong <1851050159trong@ou.edu.vn>
2021-09-29 21:19:09 +0700

    cập nhật url cho sign-in thành accounts/sign-in

--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -23,7 +23,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('',include('todoapp.urls')),
     path('admin/', admin.site.urls),
-    path('sign-in/', TokenObtainPairView.as_view(),
+    path('accounts/sign-in/', TokenObtainPairView.as_view(),
             name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(),
             name='token_refresh'),
